24fde09e7d90a1d10bf470ff47e3acafc975e349
Romayne Eastmond <romayne@gmail.com>
2024-06-01 13:56:12 -0600

    fix: Tests.

--- src/app/chat/chat.component.spec.ts
+++ src/app/chat/chat.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ChatComponent } from './chat.component';
 
 describe('ChatComponent', () => {
-  let component: ChatComponent;
-  let fixture: ComponentFixture<ChatComponent>;
+	let component: ChatComponent;
+	let fixture: ComponentFixture<ChatComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ChatComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ChatComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [ChatComponent]
+		})
+			.compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture = TestBed.createComponent(ChatComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
